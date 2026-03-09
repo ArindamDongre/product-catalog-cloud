24e4f2f343c4ab40049afa3800dcde79f203ab38
ArindamDongre <arindamdongre2253@gmail.com>
2026-03-09 14:37:18 +0530

    Add docker support

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -38,4 +39,5 @@ def add_product():
     return render_template("add_product.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
